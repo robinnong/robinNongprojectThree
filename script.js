12467b22019150732e8a75c89a7633e0940908b5
robinnong <robin.nong@hotmail.com>
2020-03-21 02:24:03 -0400

    Re-wrote function to retreive user input labels via DOM nodes

--- script.js
+++ script.js
@@ -1,26 +1,15 @@
-const legend = { 
-    rent: "thistle",
-    groceries: "powderblue",
-    utilities: "mediumslateblue",
-    fitness: "turquoise",
-    insurance: "moccasin",
-    restaurants: "lightcoral"
-} 
+// GLOBALLY DECLARED VARIABLES
+let monthly = true; // Initialize document with "Monthly View" on
+let monthlyExpenses; // Sum of monthly expenses
+let yearlyIncome; 
 
+const expenseColors = ["thistle", "powderblue", "powderblue", "mediumslateblue", "turquoise", "moccasin", "lightcoral"] 
 const tipsArray = ["The average Canadian household spends x amount on groceries per month",
                     "Test 2",
                     "Test 3",
                     "Test 4",    
-                    "Test 5",             
-                    "Test 6",
+                    "Test 5"
                     ];
-
-// GLOBALLY DECLARED VARIABLES
-let expenseLabels = Object.keys(legend); // Array of category names
-const expenseColors = Object.values(legend); // Array of colors
-let monthlyExpenses; // Sum of monthly expenses
-let monthly = true; // Initialize document with "Monthly View" on
-let yearlyIncome; 
 
 // CACHED JQUERY SELECTORS
 const $form = $('form[name="calculator"]');
@@ -48,44 +37,51 @@ const convertToString = (num) => {
         // For numbers >= $100,000 and less than $1,000,000
         array.splice(3, 0, ",");
     } // Else, do nothing
-
+    
     return array.join(""); // Returns the whole string with or w/o comma separation
+}
+
+const displayResult = (income, expenses) => {
+    const remainder = income - expenses;
+    // Properly formats dollar value strings
+    const incomeStr = convertToString(income.toFixed(2));
+    const expenseStr = convertToString(expenses.toFixed(2));
+    const remainderStr = convertToString(remainder.toFixed(2));
+    $totalIncome.text(`$${incomeStr}`);
+    $totalExpenses.text(`$${expenseStr}`);
+    $totalRemainder.text(`$${remainderStr}`);
 }
 
 // ON FORM SUBMISSION, GET USER INPUT AND DISPLAY RESULT
 const getUserInput = () => {
-    const expenseValues = []; // Array of user's expenses
-
-    $('.tip').removeClass('fadeInRight').empty();
-    $('.percentages, .percentSpend').empty(); //resets bars and text on submit
-
-    yearlyIncome = parseFloat($income.val()); // Gets user's net income
-
+    const expenseLabels = []; // Array of user's expenses (labels)
+    const expenseValues = []; // Array of user's expenses (values)
+    
+    // Creates an array of all labels (DOM elements)
+    const nodesArray = $('.expensesField label').toArray(); 
+    for (i=0; i < nodesArray.length; i++) {
+        const label = $(nodesArray[i]).attr("for"); // Access the "for" attribute of each label in the array of elements
+        expenseLabels.push(label);
+    }
+    
     // Gets value of each user input and adds it to the array of expenses
     for (i=0; i < expenseLabels.length; i++) {   
         const input = $('#' + expenseLabels[i]).val();
         const value = parseFloat(input); 
         expenseValues.push(value);
     }  
-
+    
+    yearlyIncome = parseFloat($income.val()); // Gets user's net income
     const monthlyIncome = yearlyIncome / 12;
     monthlyExpenses = expenseValues.reduce((a, b) => a + b); // Expression that returns the sum of expenses
-    const remainder = monthlyIncome - monthlyExpenses;
-
-    // Properly formats dollar value strings
-    const monthlyIncomeStr = convertToString(monthlyIncome.toFixed(2));
-    const totalExpenseStr = convertToString(monthlyExpenses.toFixed(2));
-    const remainderStr = convertToString(remainder.toFixed(2));
     
     // Displaying results for Sub-section 1
-    $totalIncome.text(`$${monthlyIncomeStr}`);
-    $totalExpenses.text(`$${totalExpenseStr}`);
-    $totalRemainder.text(`$${remainderStr}`);
+    displayResult(monthlyIncome, monthlyExpenses);
     $('.subSection1').find('.animated').addClass('fadeInUp delay-0.2s');  
-
+    
     // Displaying results for Sub-section 2
-    const percentExpense = expenseValues.map(num => ((num / monthlyIncome) * 100)); // Array of expenses as percentages
-    displaySpendingSummary(monthlyExpenses, monthlyIncome, displayBars(percentExpense));
+    const expensePercents = expenseValues.map(num => ((num / monthlyIncome) * 100)); // Array of expenses as percentages
+    displaySpendingSummary(monthlyExpenses, monthlyIncome, displayBars(expensePercents, expenseLabels));
 }   
 
 const displaySpendingSummary = (val1, val2) => {
@@ -96,7 +92,8 @@ const displaySpendingSummary = (val1, val2) => {
     $('.percentRemaining').text(`${save.toFixed(1)}%`);
     
     const div = `<div></div>`;
-    const warning = `<i class="fas fa-exclamation-circle" aria-hidden="true"></i><span> Warning: Your spending exceeds income by ${percent.toFixed(1)} times</span>`;
+    const warning = `<i class="fas fa-exclamation-circle" aria-hidden="true"></i>
+    <span> Warning: Your spending exceeds income by ${percent.toFixed(1)} times</span>`;
     
     if (spend <= 100 ) { // If spending is less or equal to 100% 
         $percentSpend.append(div).find('div').width(percent * 200); // Displays % bar at x percent
@@ -107,60 +104,51 @@ const displaySpendingSummary = (val1, val2) => {
     }
 }
 
-const displayBars = (array) => { 
-    for (i = 0; i < array.length; i++) {
-        const percent = array[i].toFixed(1)
+const displayBars = (percentArr, labelArr) => { 
+    for (i = 0; i < percentArr.length; i++) {
+        const percent = percentArr[i].toFixed(1)
         const html = `<li>
-                        <p>${expenseLabels[i]}: ${percent}%</p>
+                        <p>${labelArr[i]}: ${percent}%</p>
                         <div class="background">
-                            <div class="color"></div>
+                        <div class="color"></div>
                         </div>
                     </li>`;
 
         $('.percentages').append(html);
-        $('li:last-of-type').find('.color').width(array[i] * 0.01 * 200);
-
+        $('li:last-of-type').find('.color').width(percentArr[i] * 0.01 * 200);
+        
         if (i < expenseColors.length) {
-            const color = expenseColors[i];
-            $('li:last-of-type').find('.color').css("background-color", color);
+            $('li:last-of-type').find('.color').css("background-color", expenseColors[i]);
         } else {
             $('li:last-of-type').find('.color').css("background-color", "#9d92ff");
         }
     }
 }
 
-// Toggle between Monthly and Yearly view
+// TOGGLE BETWEEN MONTHLY & YEARLY VIEW
 const toggleViewType = () => {
-    // Locally scoped variables
     let buttonText;
     let income;  
     let expenses;
     
     if(monthly === true) { 
-        buttonText = "Yearly View"; // Changes the button text
         $toggleButton.addClass('move'); // Animates the toggle button
+        buttonText = "Yearly View"; // Changes the button text
         monthly = false; // Yearly View
-
+        
         income = yearlyIncome; // Yearly income
         expenses = monthlyExpenses * 12; // Yearly expenses
     } else { 
-        buttonText = "Monthly View"; // Changes the button text
         $toggleButton.removeClass('move'); // Animates the toggle button
+        buttonText = "Monthly View"; // Changes the button text
         monthly = true; // Monthly View
         
         income = yearlyIncome / 12; // Monthly income
         expenses = monthlyExpenses; // Monthly expenses
     }
-    const remainder = income - expenses;
-    // Properly formats dollar value strings
-    const incomeStr = convertToString(income.toFixed(2));
-    const expenseStr = convertToString(expenses.toFixed(2));
-    const remainderStr = convertToString(remainder.toFixed(2));
     
-    $viewType.text(buttonText); 
-    $totalIncome.text(`$${incomeStr}`);
-    $totalExpenses.text(`$${expenseStr}`); 
-    $totalRemainder.text(`$${remainderStr}`); 
+    displayResult(income, expenses);
+    $viewType.text(buttonText);  
 }
 
 // FORM RESET
@@ -184,7 +172,7 @@ const addNewLine = () => {
                         <input type="number" step="0.01" id="${tempLabel}" name="${tempLabel}" required="">
                     </div>
                 </div>`;
-
+                
     // Prepends the html before this div
     $('.addLine').before(html); 
 
@@ -199,7 +187,7 @@ const addNewLine = () => {
 
     // Adds this new input to the array of expenses
     expenseLabels.push(inputId);  
-    $modal.hide(); // Hides the modal
+    hideModal(); // Hides the modal
 }
 
 // DISPLAYS A RANDOM FACT
@@ -221,7 +209,8 @@ const init = () => {
     //ON MAIN FORM SUBMIT
     $form.on('submit', function(e){ 
         e.preventDefault(); 
-        $('.warning').empty();
+        $('.percentages, .percentSpend, .tip, .warning').empty(); //resets bars and text on submit
+        $('.tip').removeClass('fadeInRight');
         getUserInput();
     });
 
@@ -234,16 +223,15 @@ const init = () => {
         $newLabelId.val("");
     }); 
 
-    // //ON CLICKING LINE TO DELETE
-    // $('.expensesField > div').on('click', function () { 
-    //     $(this).toggleClass('remove'); 
-    // })
+    //ON CLICKING LINE TO DELETE
+    $('.expensesField > div').on('click', function () { 
+        $(this).toggleClass('remove'); 
+    })
 
-    // //ON CLICKING 'DELETE LINE' BUTTON
-    // $('.deleteLine').on('click', function () { 
-    //     $('.remove').remove();
-    // //delete the labels from the DOM!
-    // }); 
+    //ON CLICKING 'DELETE LINE' BUTTON
+    $('.deleteLine').on('click', function () { 
+        $('.remove').remove();
+    }); 
  
     //ON CLICKING VIEW TOGGLE BUTTON
     $toggleButton.on('click', toggleViewType);  
